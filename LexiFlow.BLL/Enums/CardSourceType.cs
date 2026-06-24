eb23e00e11b2e2e8521ecd9afca44bae70485a08
MeanLock <soryzzmikely@gmail.com>
2026-06-24 19:54:51 +0700

    add enum

---
+++ LexiFlow.BLL/Enums/CardSourceType.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LexiFlow.BLL.Enums
+{
+    public enum CardSourceType
+    {
+        Manual = 1, 
+        Dictionary = 2,
+        PublicCard = 3 
+    }
+
+}
